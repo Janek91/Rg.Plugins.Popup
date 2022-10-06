c6930056c50ffc6eb29a65dc86c77b9b85be588c
Jan Abramek <jan.abramek@tasksoftware.com>
2022-10-06 09:56:13 +0200

    Separated projects

--- Rg.Plugins.Popup/Properties/AssemblyInfo.cs
+++ Rg.Plugins.Popup/Properties/AssemblyInfo.cs
@@ -1,5 +1,8 @@
-﻿using Xamarin.Forms;
+﻿using System.Runtime.CompilerServices;
+using Xamarin.Forms;
 
 [assembly: XmlnsDefinition("http://rotorgames.com", "Rg.Plugins.Popup.Animations")]
 [assembly: XmlnsDefinition("http://rotorgames.com", "Rg.Plugins.Popup.Pages")]
 [assembly: XmlnsPrefix("http://rotorgames.com", "rg")]
+[assembly: InternalsVisibleTo("Rg.Plugins.Popup.iOS")]
+[assembly: InternalsVisibleTo("Rg.Plugins.Popup.Android")]
